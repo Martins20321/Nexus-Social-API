fc46c61e809f0d443ddf1fd390e6596029990f8f
Martins20321 <jgmsilva11@gmail.com>
2026-04-27 10:25:17 -0300

    add AuthenticationService implementing UserDetailsService and UserRepository with findByLogin method

--- src/main/java/com/martinsdev/nexussocial/api/model/User/UserRepository.java
+++ src/main/java/com/martinsdev/nexussocial/api/model/User/UserRepository.java
@@ -1,6 +1,10 @@
 package com.martinsdev.nexussocial.api.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //Performs user lookup in the database
+    UserDetails findByLogin(String login);
 }
