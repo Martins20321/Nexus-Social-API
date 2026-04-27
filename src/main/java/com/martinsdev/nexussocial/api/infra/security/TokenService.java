4431d1cc9cb33806f9a8ce981e37a13d44fa5e0d
Martins20321 <jgmsilva11@gmail.com>
2026-04-27 11:37:24 -0300

    feat: add TokenService with Auth0 JWT for authentication

---
+++ src/main/java/com/martinsdev/nexussocial/api/infra/security/TokenService.java
@@ -0,0 +1,37 @@
+package com.martinsdev.nexussocial.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.martinsdev.nexussocial.api.model.User.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secretKey}")
+    public String secretKey;
+
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.create()
+                    .withIssuer("Nexus Social API")
+                    .withSubject(user.getLogin())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error generating JWT token", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
