a5d7472ab8b9060cf081f5bf58beb8231a63c844
Martins20321 <jgmsilva11@gmail.com>
2026-04-27 10:19:16 -0300

    feat: add UserRepository

---
+++ src/main/java/com/martinsdev/nexussocial/api/model/User/UserRepository.java
@@ -0,0 +1,6 @@
+package com.martinsdev.nexussocial.api.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
