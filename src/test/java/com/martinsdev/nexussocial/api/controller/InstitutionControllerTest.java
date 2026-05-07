5d513ef4e58469f422892b8cc33ed349201caf97
Martins20321 <jgmsilva11@gmail.com>
2026-05-07 11:55:01 -0300

    test: add security context to InstitutionController tests

--- src/test/java/com/martinsdev/nexussocial/api/controller/InstitutionControllerTest.java
+++ src/test/java/com/martinsdev/nexussocial/api/controller/InstitutionControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @ActiveProfiles("test")
+@WithMockUser
 class InstitutionControllerTest {
 
     @Autowired
