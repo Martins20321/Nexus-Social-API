dd7b452628a50add894e861aaf7fe20c95d5ae56
Martins20321 <jgmsilva11@gmail.com>
2026-05-07 11:55:59 -0300

    test: add security context to NecessityController tests

--- src/test/java/com/martinsdev/nexussocial/api/controller/NecessityControllerTest.java
+++ src/test/java/com/martinsdev/nexussocial/api/controller/NecessityControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureJsonTesters
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@WithMockUser
 class NecessityControllerTest {
 
     @Autowired
