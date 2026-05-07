0b4b33ab9fa8a88c862a064f9f305d038811d0e6
Martins20321 <jgmsilva11@gmail.com>
2026-05-07 11:51:17 -0300

    test: add security context to AddressController tests

--- src/test/java/com/martinsdev/nexussocial/api/controller/AddressControllerTest.java
+++ src/test/java/com/martinsdev/nexussocial/api/controller/AddressControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @ActiveProfiles("test")
+@WithMockUser
 class AddressControllerTest {
 
     @Autowired
