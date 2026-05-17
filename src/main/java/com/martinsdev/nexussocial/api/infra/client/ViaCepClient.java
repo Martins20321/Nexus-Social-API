2c2fc33d16af72e00a66ac0e3242edff6e5edf20
Martins20321 <jgmsilva11@gmail.com>
2026-05-17 19:06:34 -0300

    feat(infra): add ViaCepClient for address enrichment via ViaCep API

---
+++ src/main/java/com/martinsdev/nexussocial/api/infra/client/ViaCepClient.java
@@ -0,0 +1,29 @@
+package com.martinsdev.nexussocial.api.infra.client;
+
+import com.martinsdev.nexussocial.api.dto.ViaCepResponseDTO;
+import com.martinsdev.nexussocial.api.infra.exception.InvalidZipCodeException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class ViaCepClient {
+
+    private final RestClient restClient;
+
+    public ViaCepClient() {
+        this.restClient = RestClient.create("https://viacep.com.br/ws");
+    }
+
+    public ViaCepResponseDTO findAddressByZipCode(String zipCode) {
+        ViaCepResponseDTO response = restClient.get()
+                .uri("/{zipCode}/json", zipCode)
+                .retrieve()
+                .body(ViaCepResponseDTO.class);
+
+        if (response.erro()){
+            throw new InvalidZipCodeException(zipCode);
+        }
+
+        return response;
+    }
+}
