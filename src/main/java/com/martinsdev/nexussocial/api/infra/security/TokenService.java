8659eb084fb8e36bc82e678b81db47dbc5d39e86
Martins20321 <jgmsilva11@gmail.com>
2026-05-26 14:48:42 -0300

    feat: persist refresh token on generation in TokenService

--- src/main/java/com/martinsdev/nexussocial/api/infra/security/TokenService.java
+++ src/main/java/com/martinsdev/nexussocial/api/infra/security/TokenService.java
@@ -4,7 +4,10 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.martinsdev.nexussocial.api.model.refreshtoken.RefreshToken;
+import com.martinsdev.nexussocial.api.model.refreshtoken.RefreshTokenRepository;
 import com.martinsdev.nexussocial.api.model.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +16,14 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 @Service
+@RequiredArgsConstructor
 public class TokenService {
 
     @Value("${api.security.token.secretKey}")
     public String secretKey;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    public String generateToken(User user){
+    public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
             return JWT.create()
@@ -27,12 +32,29 @@ public class TokenService {
                     .withClaim("id", user.getId())
                     .withExpiresAt(expirationDate())
                     .sign(algorithm);
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             throw new RuntimeException("Error generating JWT token", exception);
         }
     }
 
-    public String getSubject(String tokenJWT){
+    public String generateRefreshToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            Instant expiration = expirationDateRefreshToken();
+            var refresthToken = JWT.create()
+                    .withIssuer("Nexus Social API")
+                    .withSubject(user.getId().toString())
+                    .withExpiresAt(expiration)
+                    .sign(algorithm);
+            refreshTokenRepository.save(new RefreshToken(null, refresthToken, false,
+                    LocalDateTime.ofInstant(expiration, ZoneOffset.of("-03:00")), user));
+            return refresthToken;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error generating Refresh token", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
             return JWT.require(algorithm)
@@ -40,12 +62,17 @@ public class TokenService {
                     .build()
                     .verify(tokenJWT)
                     .getSubject();
-        } catch (JWTVerificationException exception){
+        } catch (JWTVerificationException exception) {
             throw new RuntimeException("Invalid or expired JWT token");
         }
     }
 
     private Instant expirationDate() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+
+    private Instant expirationDateRefreshToken() {
+        return LocalDateTime.now().plusDays(7).toInstant(ZoneOffset.of("-03:00"));
     }
 }
